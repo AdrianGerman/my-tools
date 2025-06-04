c6133cdaa6132efe9c878d2409c514093a327333
AdrianGerman <adrian.german1019@gmail.com>
2025-06-04 08:55:36 -0600

    background page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 function App() {
   return (
-    <main className="h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-edu">
-        Hello, <span className="font-bold">from my app</span>
-      </h1>
+    <main className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-radial-glow">
+      <div className="flex h-full w-full items-center justify-center">
+        <h1 className="text-4xl font-edu">
+          Hello, <span className="font-bold">from my app</span>
+        </h1>
+      </div>
     </main>
   )
 }
