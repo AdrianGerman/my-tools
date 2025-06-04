3087544cef9824a9d21ba7686353e5225a6a202d
AdrianGerman <adrian.german1019@gmail.com>
2025-06-04 08:51:10 -0600

    title font

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <main className="h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Hello, from my app</h1>
+      <h1 className="text-4xl font-edu">
+        Hello, <span className="font-bold">from my app</span>
+      </h1>
     </main>
   )
 }
