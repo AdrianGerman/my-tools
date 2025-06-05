daaff16a5d9e7c9ea852e5e8379dbf8f1a044146
AdrianGerman <adrian.german1019@gmail.com>
2025-06-05 06:37:12 -0600

    info tool component and new font

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { InfoTools } from "./components/InfoTools"
+
 function App() {
   return (
     <main className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-radial-glow">
-      <div className="flex h-full w-full items-center justify-center">
-        <h1 className="text-4xl font-edu">
+      <div className="flex flex-col w-full justify-center items-center">
+        <h1 className="text-5xl font-edu py-12">
           Hello, <span className="font-bold">from my app</span>
         </h1>
+        <InfoTools />
       </div>
     </main>
   )
---
+++ src/components/InfoTools.jsx
@@ -0,0 +1,10 @@
+export const InfoTools = () => {
+  return (
+    <>
+      <section className="font-inter py-12">
+        <p>Web que recopila herramientas útiles para tu día a día</p>
+        <p>Todo lo que puedas necesitar en una sola pagina web</p>
+      </section>
+    </>
+  )
+}
